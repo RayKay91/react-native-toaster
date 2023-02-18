7dd67a60c50aaa46aec06773e3f5d2ffaf132b2c
RayKay91 <r61206305+RayKay91@users.noreply.github.com>
2023-02-18 16:50:32 +0000

    chore: get toast component working

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,36 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-toaster';
+import { StyleSheet, View, Button, Text } from 'react-native';
+import { Toast } from 'react-native-toaster';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+function App() {
+  const [show, setShow] = React.useState(false);
+  const [t, setT] = React.useState('hello :)');
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Toast
+        containerStyle={{ backgroundColor: 'red' }}
+        isVisible={show}
+        setIsVisible={(s) => setShow(s)}
+        delay={5000}
+        onWillShow={() => setT('will show')}
+        onDidShow={() => setT('did show')}
+        onWillHide={() => setT('will hide')}
+        onDidHide={() => setT('did hide')}
+      />
+      <Button title="lol" onPress={() => setShow(true)} />
+      <Text>{t}</Text>
     </View>
   );
 }
+
+export default () => (
+  <GestureHandlerRootView style={{ flex: 1 }}>
+    <App />
+  </GestureHandlerRootView>
+);
 
 const styles = StyleSheet.create({
   container: {
