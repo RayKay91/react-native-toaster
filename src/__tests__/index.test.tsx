2eb544cc1663cfb8510aee97876e016db50697b6
RayKay91 <r61206305+RayKay91@users.noreply.github.com>
2023-03-20 20:32:31 +0000

    test: update tests

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Toast } from '..';
-import { render, screen } from '@testing-library/react-native';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+} from '@testing-library/react-native';
 import '@testing-library/jest-native/extend-expect';
 
 describe('<Toast />', () => {
@@ -42,5 +47,64 @@ describe('<Toast />', () => {
     render(<Toast {...props} />);
     const toast = await screen.getByTestId(/toast/);
     expect(toast).not.toBeVisible();
+  });
+
+  it('should render the title and subtext', () => {
+    const mockProps = {
+      title: 'hello',
+      subText: 'world',
+      setIsVisible: jest.fn(),
+      isVisible: false,
+      displayNextToastInQueue: jest.fn(),
+    };
+    const { getByText } = render(<Toast {...mockProps} />);
+
+    expect(getByText('hello')).toBeDefined();
+    expect(getByText('world')).toBeDefined();
+  });
+
+  it('should call onPress() and then call setIsVisible(false)', () => {
+    const mockProps = {
+      title: 'hello',
+      subText: 'world',
+      setIsVisible: jest.fn(),
+      isVisible: false,
+      displayNextToastInQueue: jest.fn(),
+      onPress: jest.fn(),
+    };
+    render(<Toast {...mockProps} />);
+    const toast = screen.getByRole('button');
+    fireEvent.press(toast);
+    expect(mockProps.onPress).toBeCalledTimes(1);
+    expect(mockProps.setIsVisible).toBeCalledTimes(1);
+    expect(mockProps.setIsVisible).toBeCalledWith(false);
+  });
+  // it('should render the title and subtext', () => {
+  //   const mockProps = {
+  //     title: 'hello',
+  //     subText: 'world',
+  //     setIsVisible: jest.fn(),
+  //     isVisible: false,
+  //     displayNextToastInQueue: jest.fn(),
+  //   };
+  //   const { getByText } = render(<Toast {...mockProps} />);
+
+  //   expect(getByText('hello')).toBeDefined();
+  //   expect(getByText('world')).toBeDefined();
+  // });
+
+  // this test causes weird open handles issue due to animation timer not being properly run
+  // using jest --forceExit to workaround for now.
+  it('is on screen if is visible', async () => {
+    const props = {
+      title: 'hello',
+      subText: 'world',
+      setIsVisible: jest.fn(),
+      isVisible: true,
+      displayNextToastInQueue: jest.fn(),
+    };
+    render(<Toast {...props} />);
+    const toast = await screen.getByTestId(/toast/);
+    await waitFor(() => expect(toast).toBeVisible());
   });
 });
